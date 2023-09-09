0ef96cb18f8a15b992e23de7614438771a8ee3f8
Clement-Lotus-Mollick <augustempire999@gmail.com>
2023-09-09 14:51:22 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+
+//   thmes 
+
+module.exports = {
+    //...
+    daisyui: {
+      themes: ["light", "dark", "cupcake"],
+    },
+  }
